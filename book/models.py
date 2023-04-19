4731be3660fb15fce2cc5c2379f89389c486ac83
马佳文 <mjw@bupt.edu.cn>
2023-04-19 10:30:39 +0800

    添加模板

--- book/models.py
+++ book/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class BookInfo(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
 
 class PeopleInfo(models.Model):
     name = models.CharField(max_length=10)
--- book/views.py
+++ book/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+'''
+视图函数
+'''
+
+def index(request):
+    # return HttpResponse('OK!')
+
+    context = {
+        'name' : '马上双十一，点我有惊喜',
+        'bookname' : '西游记'
+    }
+
+    return render(request, 'book/index.html', context=context)
